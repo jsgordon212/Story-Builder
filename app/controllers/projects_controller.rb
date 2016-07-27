2cd24450448f836f7d1178d5aa291f84ec0f24d9
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-07-26 22:55:43 -0400

    work on projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(post_params)
+    @project = Project.new(project_params)
     @project.user_id = @current_user.id if @current_user
 
     if @project.save
