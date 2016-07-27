aa9f0a177bde3ee7c8f1f35f7b17ee52d1d37af1
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-07-27 19:13:37 -0400

    probably very buggy seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 users = [User.create(username: "jared", email: "jared@jared.com", password: "Password1"),User.create(username: "katbroner", email: "katbroner@jared.com", password: "Password1"),User.create(username: "samanthavholmes", email: "samanthavholmes@jared.com", password: "Password1"),User.create(username: "efitz", email: "efitz@jared.com", password: "Password1"),User.create(username: "user1234", email: "user1234@gmail.com", password: "Password1"),User.create(username: "usernameusername", email: "auser@gmail.com", password: "Password1"),User.create(username: "jared2", email: "jared2@gmail.com", password: "Password1")]
 
-450.times do
+500.times do
 	Project.create(title: Faker::Hipster.sentence, user: users.sample, summary: Faker::Hipster.paragraph([3..9].sample, true))
 end
 
@@ -17,19 +17,24 @@ end
 end
 
 970.times do
-	Chapter.create(projet: Project.all.sample, chapter_number: rand(1..63), chapter_title: Faker::Hipster.sentence, summary: Faker::Hipster.paragraph([2..8].sample, true))
+	Chapter.create(project: Project.all.sample, chapter_number: rand(1..63), chapter_title: Faker::Hipster.sentence, summary: Faker::Hipster.paragraph([2..8].sample, true))
 end
 
 1020.times do
-	PlotPoint.create(main_plot: [true, false].sample, project: Project.all.sample, description: Faker::Hipster.sentences([1..5].sample).join(' '), description: Faker::Hipster.paragraph([2..8].sample, true))
+	PlotPoint.create(main_plot: [true, false].sample, project: Project.all.sample, description: Faker::Hipster.paragraph([2..8].sample, true))
 end
 
+1007.times do
+	Project.all.sample.chapters << Chapter.all.sample
 
+	Project.all.sample.characters << Character.all.sample
 
+	Project.all.sample.plot_points << PlotPoint.all.sample
 
+	Chapter.all.sample.plot_points << PlotPoint.all.sample
 
-
-
-
+	Character.all.sample.chapters << Chapter.all.sample
+end
+# Chapter.all.sample.charaters << Character.all.sample
 
 
