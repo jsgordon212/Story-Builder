00eb818429d357e24eb8c68d449d4730fc3d8d57
Katherine Broner <katherine.broner@gmail.com>
2016-07-26 22:59:31 -0400

    update projects page

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
+  belongs_to :project
   has_many :chapter_characters
   has_many :chapters, through: :chapter_characters
-  belongs_to :project
   validates :name, presence: true
 end
--- db/migrate/20160726235433_create_characters.rb
+++ db/migrate/20160726235433_create_characters.rb
@@ -1,9 +1,9 @@
 class CreateCharacters < ActiveRecord::Migration[5.0]
   def change
     create_table :characters do |t|
+      t.references :project, foreign_key: true, index: true
       t.string :name, null: false
       t.string :description
-      t.references :project, null: false
 
       t.timestamps null: false
     end
