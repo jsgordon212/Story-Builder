3a66208567ff797df00cae9cdb0fa1c37214e8e4
jsgordon212 <jsgordon212@gmail.com>
2016-07-26 20:39:09 -0400

    adds resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show]
+  resources :projects
+  resources :chapters
+  resources :characters
+  resources :plot_points
+
 end
