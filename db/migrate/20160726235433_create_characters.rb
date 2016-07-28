6c3d15a845433b48e5db8f940432f23ecafb7dab
Samantha Holmes <samanthavholmes@gmail.com>
2016-07-28 13:55:40 -0400

    take out chapter id from character and make seed file smaller

--- db/migrate/20160726235433_create_characters.rb
+++ db/migrate/20160726235433_create_characters.rb
@@ -2,7 +2,6 @@ class CreateCharacters < ActiveRecord::Migration[5.0]
   def change
     create_table :characters do |t|
       t.references :project, foreign_key: true, index: true
-      t.references :chapter, foreign_key: true
       t.string :name, null: false
       t.string :description
 
